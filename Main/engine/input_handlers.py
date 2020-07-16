c65b5b56a2ba8c765bae034c7da37596ffeabef6
Zedalgo <xero321@verizon.net>
2020-07-16 18:21:41 -0400

    Removed alternate movement keys bc they were bad Updated README

--- Main/engine/input_handlers.py
+++ Main/engine/input_handlers.py
@@ -31,13 +31,13 @@ def handle_player_turn_keys(key):
     key_char = chr(key.c)
 
     # Movement keys
-    if key.vk == libtcod.KEY_UP or key_char == 'k':
+    if key.vk == libtcod.KEY_UP:
         return {'move': (0, -1)}
-    elif key.vk == libtcod.KEY_DOWN or key_char == 'j':
+    elif key.vk == libtcod.KEY_DOWN:
         return {'move': (0, 1)}
-    elif key.vk == libtcod.KEY_LEFT or key_char == 'h':
+    elif key.vk == libtcod.KEY_LEFT:
         return {'move': (-1, 0)}
-    elif key.vk == libtcod.KEY_RIGHT or key_char == 'l':
+    elif key.vk == libtcod.KEY_RIGHT:
         return {'move': (1, 0)}
     elif key_char == 'y':
         return {'move': (-1, -1)}
--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-# Scroll of Symbol Guidance
+# Scroll of Guidance
 
+Keybinds
+* Cardinal Movement: Arrow Keys
+* Diagonal Movement: yubn
+* Wait: z
+* Inventory: i
+* Pickup/Get: g
+* Drop: d
+* Use Stairs: ENTER
+* Character Screen: c
+* Main Menu/Exit Game: ESC
+* Fullscreen: ALT+ENTER
+
+Symbol Guide
 * Corpse %
 * Dagger -
 * Potion !
